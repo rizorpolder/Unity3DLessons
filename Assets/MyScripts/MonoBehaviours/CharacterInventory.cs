398872d7c91d4035e51a5f8d4634f826bb43d698
Кирилл Князев <rizorpolder@gmail.com>
2019-02-07 22:48:13 +0300

    Inventory

--- Assets/MyScripts/MonoBehaviours/CharacterInventory.cs
+++ Assets/MyScripts/MonoBehaviours/CharacterInventory.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterInventory : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static CharacterInventory instance;
+    public CharacterStats charStats;
+
+    public Image[] hotBarDisplayHolders = new Image[4];
+    public GameObject InventoryDisplayHolder;
+    public Image[] inventoryDisplaySlots = new Image[30];
+
+    int inventoryItempCap  =20;
+    int idCount =1;
+    bool addedItem = true;
+
+    public Dictionary<int,InventoryEntry> itemsInInventory = new Dictionary<int, InventoryEntry>();
+    public InventoryEntry itemEntry;
     void Start()
     {
-        
+        instance = this;
+
+        itemEntry=new InventoryEntry(0,null,null);
+        itemsInInventory.Clear();
+        inventoryDisplaySlots = InventoryDisplayHolder.GetComponentsInChildren<Image>();
+
+        charStats = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStats>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StoreItem(ItemPickUp ItemToStore)
     {
         
+
+
     }
 }
